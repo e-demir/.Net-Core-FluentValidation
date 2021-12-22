8486d4e9bf27f62e3bd81a0f5eea924f6e1a5584
Emrullah DEMİR <e177demir@gmail.com>
2021-12-23 00:21:20 +0300

    Initial comment
    A short example about Fluent Validation on Asp .Net Core API.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidationExample.Models;
+using FluentValidationExample.Validators;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FluentValidationExample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(LoginRequestModel request)
+        {
+            var validator = new LoginValidator();
+            var validationResult = await validator.ValidateAsync(request);
+            if (!validationResult.IsValid)
+            {                
+                return StatusCode(StatusCodes.Status500InternalServerError, new { ErrorMessage = string.Join(" - ", validationResult.Errors) });
+            }
+
+            return Ok(request);
+        }
+    }
+}
